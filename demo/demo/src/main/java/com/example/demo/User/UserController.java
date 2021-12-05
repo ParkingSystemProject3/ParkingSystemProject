52851b81d239ed241c6c6fa471b6782f91e2ba85
shahad96 <shahad96.777@gmail.com>
2021-12-05 16:39:54 +0300

    frontend add user get spot

--- demo/demo/src/main/java/com/example/demo/User/UserController.java
+++ demo/demo/src/main/java/com/example/demo/User/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000/")
 @RestController
 @RequestMapping(path="users")
 public class UserController {
