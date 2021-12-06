127a55c31e4f9814ff684e13a2a556d7a33961e4
shahad96 <shahad96.777@gmail.com>
2021-12-06 10:36:24 +0300

    frontend popup

--- demo/demo/src/main/frontEnd/parkingsystem/src/components/Entry.js
+++ demo/demo/src/main/frontEnd/parkingsystem/src/components/Entry.js
@@ -1,20 +1,20 @@
 import axios from "axios";
+import React, { useState } from 'react';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 
 function Entry() {
   let slotType;
-  let data = { id: 0 };
+  const[id,setId]=useState(0);
 
-  function getSpot(event) {
-    event.preventDefault();
+  function getSpot() {
     console.log("in");
     if (slotType == "normal") {
       console.log("inin");
       axios
         .get("http://localhost:8080/spot/normal")
         .then(function (response) {
-          data = response.data.id;
-          alert(" normal " + response.id);
-          console.log(response.id);
+          setId(response.data.id);
         })
         .catch(function (error) {
           console.error(error);
@@ -23,9 +23,7 @@ function Entry() {
       axios
         .get("http://localhost:8080/spot/not_normal")
         .then(function (response) {
-          console.log(response.data.id);
-          data = response.data.id;
-          alert(" the spot emity is " + data);
+          setId(response.data.id);
         })
         .catch(function (error) {
           console.error(error);
@@ -68,6 +66,7 @@ function Entry() {
           name="Special Needs"
           onClick={() => {
             slotType = "normal";
+            getSpot();
           }}
           value="Special Needs"
         />
@@ -81,26 +80,30 @@ function Entry() {
           value="Special Needs"
           onClick={() => {
             slotType = "Notnormal";
-            console.log("ff");
+            getSpot();
           }}
         />
       </div>
       <div className="group">
-        <input
-          onClick={getSpot}
+        <Popup trigger={<input
           type="submit"
           className="button"
           defaultValue="Sign In"
-        />
+        />} position="right center">
+    <div>
+    <h5>{id} spot is empty</h5>
+    </div>
+  </Popup>
       </div>
-
+      <br></br>
+        
       <div className="group">
-        <label className="label">First name22</label>
+        <label className="label">First name</label>
         <input type="text" id="fname" className="input" />
       </div>
       <div className="group">
         <label className="label">Last name</label>
-        <input type="text" id="fname" className="input" />
+        <input type="text" id="lname" className="input" />
       </div>
       <div className="group">
         <label className="label">Plat Number</label>
--- demo/demo/src/main/frontEnd/parkingsystem/src/components/Exit.js
+++ demo/demo/src/main/frontEnd/parkingsystem/src/components/Exit.js
@@ -1,23 +1,29 @@
 import axios from "axios";
-import React from 'react';
+import React, { useState } from 'react';
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 
 function Exit() {
   // هذا المتغير يكون فيه معلومات التكت اللي تنطبع على الشاشة
-  let ticket;
+  let m ={endTime:"",id:"",price:"",startTime:0};
+  const [ticket,setTicket]=useState(m);
+  const[plat,setPlat]=useState(0);
 
   function ExitUser(event) {
     event.preventDefault();
-    let pn = document.getElementById("platNum").value;
+    // setPlat(document.getElementById("platnum").value);
+    setPlat(document.getElementById("platnum").value);
     console.log("in pn");
     axios
-      .put(`http://localhost:8080/ticket/${pn}`)
+      .put(`http://localhost:8080/ticket/${plat}`)
       .then(function (response) {
         axios
-          .get(`http://localhost:8080/ticket/${pn}`)
+          .get(`http://localhost:8080/ticket/${plat}`)
           .then(function (response) {
-            ticket = response.data;
+            setTicket(response.data);
+            console.log(response.data);
+            
+              
           })
           .catch(function (error) {
             console.error(error);
@@ -40,6 +46,15 @@ function Exit() {
           className="button"
           defaultValue="Sign Up"
         />
+        <br></br>
+        <Popup trigger={<button className="button">Print ticket</button>} position="bottom center">
+    <div>
+    <h5>Plat number:{plat}</h5>
+        <h5>Entry Time:{ticket.startTime}</h5>
+    <h5>Exit Time:{ticket.endTime}</h5>
+    <h5>Total price:{ticket.price}</h5>
+    </div>
+  </Popup>
       </div>
     </div>
   );
